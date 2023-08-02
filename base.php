fd097e0c6bf9ec4e0b7622e4e2133feadc70dbe6
jerrywu <s736828@gmail.com>
2023-08-02 16:27:12 +0800

    0802

---
+++ base.php
@@ -0,0 +1,32 @@
+<?php
+$BASEDIR = __DIR__;
+session_start();
+
+include_once $BASEDIR . "/Controller/Ad.php";
+include_once $BASEDIR . "/Controller/Admin.php";
+include_once $BASEDIR . "/Controller/Bottom.php";
+include_once $BASEDIR . "/Controller/Image.php";
+include_once $BASEDIR . "/Controller/Menu.php";
+include_once $BASEDIR . "/Controller/Mvim.php";
+include_once $BASEDIR . "/Controller/News.php";
+include_once $BASEDIR . "/Controller/Title.php";
+include_once $BASEDIR . "/Controller/Total.php";
+
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "</pre>";
+}
+function to($url)
+{
+    header("location:" . $url);
+}
+
+function q($sql)
+{
+    $pdo = new PDO("mysql:host=localhost;charset=utf8;dbname=db01,'root',''");
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
